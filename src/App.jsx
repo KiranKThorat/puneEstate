443fde982264e68c17a6782ee4fad2af1dc71bc0
kt <kthorat158@gmail.com>
2024-02-07 20:06:44 +0530

    installed tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      
+<h1 className='text-red-700'>hiii</h1>
+<h1 className='text-red-700'>KIran</h1>
+
+
+    </div>
+  )
+}
