8cf996387c94b353e6d68a29f3eb5de293516f07
kt <kthorat158@gmail.com>
2024-02-13 18:08:39 +0530

    signup page created and handled neccessary changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
  <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/signin" element={<Signin/>} />
-  <Route path="/signout" element={<Signout/>} />
+  <Route path="/signup" element={<Signout/>} />
   <Route path="/profile" element={<Profile />} />
   <Route path="/about" element={<About/>} />
 
